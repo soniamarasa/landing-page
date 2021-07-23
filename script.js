4d526ae4118cea408248e3b628cb914e50a9d30c
Sonia Mara De Sá <soniamarasa@hotmail.com>
2021-07-23 14:51:19 -0300

    formatação

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ let days, hours, minutes, seconds;
 window.addEventListener('load', () => {
   dates();
 
-  submit.addEventListener('click', (e) => {
+  submit.addEventListener('submit', (e) => {
     e.preventDefault();
 
     const user = { name: inputName.value, email: inputEmail.value };
